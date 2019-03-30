0588808a570258233477b0c3104a5661f5b2d7ca
Anayo Oleru <anayooleru@Anayos-MacBook-Pro.local>
2019-03-30 09:07:07 +0100

    add data

---
+++ src/Data/codesData.js
@@ -0,0 +1,47 @@
+const codesData = [
+  {
+    id: 1111,
+    errorCode: '403',
+    errorMsg: 'Inavlid username and password',
+    tags: ['403',
+      'username',
+    ],
+  },
+
+  // {
+  //     "id":1112,
+  //     "errorCode":,
+  //     "errorMsg":,
+  //     "tags":,
+  // },
+
+  // {
+  //     "id":1113,
+  //     "errorCode":,
+  //     "errorMsg":,
+  //     "tags":,
+  // },
+
+  // {
+  //     "id":1114,
+  //     "errorCode":,
+  //     "errorMsg":,
+  //     "tags":,
+  // },
+
+  // {
+  //     "id":1115,
+  //     "errorCode":,
+  //     "errorMsg":,
+  //     "tags":,
+  // },
+
+  // {
+  //     "id":1116,
+  //     "errorCode":,
+  //     "errorMsg":,
+  //     "tags":,
+  // }
+];
+
+export default codesData;
---
+++ src/Data/setupsData.js
@@ -0,0 +1,47 @@
+const setupsData = [
+  {
+    id: 1111,
+    title: 'How to setup Eslint',
+    body: 'Eslint is good for Linting',
+    tags: ['eslint',
+      'setups',
+    ],
+  },
+
+  // {
+  //     "id":1112,
+  //     "errorCode":,
+  //     "errorMsg":,
+  //     "tags":,
+  // },
+
+  // {
+  //     "id":1113,
+  //     "errorCode":,
+  //     "errorMsg":,
+  //     "tags":,
+  // },
+
+  // {
+  //     "id":1114,
+  //     "errorCode":,
+  //     "errorMsg":,
+  //     "tags":,
+  // },
+
+  // {
+  //     "id":1115,
+  //     "errorCode":,
+  //     "errorMsg":,
+  //     "tags":,
+  // },
+
+  // {
+  //     "id":1116,
+  //     "errorCode":,
+  //     "errorMsg":,
+  //     "tags":,
+  // }
+];
+
+export default setupsData;
